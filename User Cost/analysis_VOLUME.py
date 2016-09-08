d20b9d7ddb7db637702858d91f227b97ff3e8ae2
Dan Olejarz <dan.olejarz@gmail.com>
2016-09-08 15:22:19 -0400

    Adheres to PEP8

--- User Cost/analysis_VOLUME.py
+++ User Cost/analysis_VOLUME.py
@@ -10,7 +10,8 @@ import matplotlib
 matplotlib.style.use('seaborn-whitegrid')
 
 
-engine = create_engine('postgresql://dolejar:dan@137.15.155.38:5432/bigdata_ah')
+engine = create_engine('postgresql://'\
+'dolejar:dan@137.15.155.38:5432/bigdata_ah')
 
 segments=['A','B','B1','C','D','E','F','G','H','I','J','K']
 dvpSegmentsN = ['F','G','H','I','J','K']
@@ -20,25 +21,59 @@ fggSegmentsW = list(reversed(fggSegmentsE))
 
 def readSegments(inSegments, month, day, n, engine):
     '''
-    Returns list of dataframes containing bliptrack travel times for n days on inputted segments
+    Returns list of dataframes 
+    Each element is travel time on one segment
     '''
     dfList = []
     startDate = datetime.date(2015,month,day)
     endDate = startDate + timedelta(days=n)
-    strStartDate = startDate.strftime('%Y-%m-%d')
-    strEndDate = endDate.strftime('%Y-%m-%d')
+    
     for i in range(len(inSegments)-1):
         print('Query going in...')
-        strSQL = 'SELECT bluetooth.raw_data."Timestamp",bluetooth.raw_data."AvgMeasuredTime",bluetooth.raw_data."StartPointName",bluetooth.raw_data."EndPointName" FROM bluetooth.raw_data INNER JOIN bluetooth.ref_segments ON (bluetooth.raw_data."StartPointName" = bluetooth.ref_segments.orig_startpointname AND bluetooth.raw_data."EndPointName" = bluetooth.ref_segments.orig_endpointname) WHERE (bluetooth.ref_segments.startpointname = \''+inSegments[i]+'\' AND bluetooth.ref_segments.endpointname = \''+inSegments[i+1]+'\' AND bluetooth.raw_data."Timestamp" >= \''+strStartDate+'\' AND bluetooth.raw_data."Timestamp" < \''+strEndDate+'\')'
-        dfList.append(pandasql.read_sql(strSQL,engine))
+        strSQL = 'SELECT bluetooth.raw_data."Timestamp",'\
+        'bluetooth.raw_data."AvgMeasuredTime",'\
+        'bluetooth.raw_data."StartPointName",'\
+        'bluetooth.raw_data."EndPointName" '\
+        'FROM bluetooth.raw_data INNER JOIN bluetooth.ref_segments '\
+        'ON (bluetooth.raw_data."StartPointName" '\
+        '= bluetooth.ref_segments.orig_startpointname '\
+        'AND bluetooth.raw_data."EndPointName" '\
+        '= bluetooth.ref_segments.orig_endpointname) '\
+        'WHERE (bluetooth.ref_segments.startpointname = %(pt1)s '\
+        'AND bluetooth.ref_segments.endpointname = %(pt2)s '\
+        'AND bluetooth.raw_data."Timestamp" >= %(dstart)s '\
+        'AND bluetooth.raw_data."Timestamp" < %(dend)s)'
+        dfList.append(pandasql.read_sql(sql=strSQL,
+                                        params={'pt1':inSegments[i],
+                                                'pt2':inSegments[i+1],
+                                                'dstart':startDate,
+                                                'dend':endDate},
+                                        con=engine))
     return dfList
     
-def readVolumes(month, day, engine):
+def readVolumes(startDate, engine):
     
-    startDate = datetime.date(2015,month,day)
-    strStartDate = startDate.strftime('%Y-%m-%d')
-    #strSQL = 'SELECT * FROM cot.volumes WHERE(cot.volumes.count_date LIKE \'%GARDINER%\' AND cot.volumes.count_date >= \''+strStartDate+'\' AND cot.volumes.count_date < \''+strEndDate+'\')'
-    #strSQL = 'SELECT * FROM cot.volumes WHERE (cot.volumes.count_date = \''+strStartDate+'\' AND cot.volumes.location LIKE \' \%GARDINER\% \')' 
-    strSQL = 'SELECT * FROM cot.volumes WHERE (cot.volumes.count_date = %(dstart)s AND cot.volumes.location LIKE %(road)s)'     
-    df = pandasql.read_sql(sql=strSQL,params={"dstart":startDate,"road":"%GARDINER%"},con=engine)
+    #startDate = datetime.date(2014,month,day)
+    strSQL = 'SELECT * '\
+    'FROM cot.volumes '\
+    'WHERE (cot.volumes.count_date = %(dstart)s ' \
+    'AND cot.volumes.location LIKE %(road)s)'     
+    df = pandasql.read_sql(sql=strSQL,
+                           params={'dstart':startDate,
+                                   'road':'%GARDINER%'},
+                           con=engine)
     return df    
+
+'''    
+i = 0
+startDate = datetime.date(2015,1,1)
+emptyList = []
+
+while i < 365:
+    print(i)
+    df = readVolumes(startDate,engine)
+    if df.empty == True:
+        emptyList.append(startDate)
+    startDate += timedelta(days=1)
+    i += 1
+'''
